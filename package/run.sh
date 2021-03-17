dc9c05e1177283fbb176aa2b21bb32325a6baefa
Chris Kim <oats87g@gmail.com>
2021-03-17 13:52:25 -0700

    add initial image for installing rke2 using local artifacts
    Signed-off-by: Chris Kim <oats87g@gmail.com>

--- package/run.sh
+++ package/run.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-./install.sh
+set -x -e
+
+env "INSTALL_RKE2_ARTIFACT_PATH=${CATTLE_AGENT_EXECUTION_PWD}" installer.sh
+if [ -z "${INSTALL_RKE2_TYPE}" ]; then
+    INSTALL_RKE2_TYPE="server"
+fi
+systemctl daemon-reload
+systemctl enable rke2-${INSTALL_RKE2_TYPE}
+systemctl start rke2-${INSTALL_RKE2_TYPE}
