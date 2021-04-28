892dc491ab3bd22eeca42f383f95c896ea665140
Chris Kim <oats87g@gmail.com>
2021-04-28 08:38:17 -0700

    Add INSTALL_RKE2_SKIP_START and INSTALL_RKE2_SKIP_ENABLE environment variables
    Signed-off-by: Chris Kim <oats87g@gmail.com>

--- package/run.sh
+++ package/run.sh
@@ -6,6 +6,13 @@ env "INSTALL_RKE2_ARTIFACT_PATH=${CATTLE_AGENT_EXECUTION_PWD}" installer.sh
 if [ -z "${INSTALL_RKE2_TYPE}" ]; then
     INSTALL_RKE2_TYPE="server"
 fi
+
 systemctl daemon-reload
+if [ "${INSTALL_RKE2_SKIP_ENABLE}" = true ]; then
+    exit 0
+fi
 systemctl enable rke2-${INSTALL_RKE2_TYPE}
+if [ "${INSTALL_RKE2_SKIP_START}" = true ]; then
+    exit 0
+fi
 systemctl restart rke2-${INSTALL_RKE2_TYPE}
