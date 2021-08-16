9c7e2b5a59f9aef579b5e0aaaaff041f6ba39e7b
Brian Downs <brian.downs@gmail.com>
2021-08-16 13:20:03 -0700

    add ability to set env vars for rke2
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- package/run.sh
+++ package/run.sh
@@ -5,6 +5,7 @@ set -x -e
 mkdir -p /var/lib/rancher/rke2
 
 RESTART_STAMP_FILE=/var/lib/rancher/rke2/restart_stamp
+RKE2_ENV_FILE=/usr/local/lib/systemd/system/rke2-server.env
 
 if [ -f "${RESTART_STAMP_FILE}" ]; then
     PRIOR_RESTART_STAMP=$(cat "${RESTART_STAMP_FILE}");
@@ -17,6 +18,13 @@ else
 fi
 
 env "INSTALL_RKE2_ARTIFACT_PATH=${CATTLE_AGENT_EXECUTION_PWD}" installer.sh
+
+if [ ! -f ${RKE2_ENV_FILE} ]; then
+    install -m 600 /dev/null ${RKE2_ENV_FILE}
+fi
+
+env | grep '^RKE2_' | tee -a ${RKE2_ENV_FILE} >/dev/null
+env | grep -Ei '^(NO|HTTP|HTTPS)_PROXY' | tee -a ${RKE2_ENV_FILE} >/dev/null
 
 if [ -z "${INSTALL_RKE2_TYPE}" ]; then
     INSTALL_RKE2_TYPE="${INSTALL_RKE2_EXEC:-server}"
@@ -32,12 +40,12 @@ if [ "${INSTALL_RKE2_SKIP_ENABLE}" = true ]; then
     exit 0
 fi
 
-systemctl enable rke2-${INSTALL_RKE2_TYPE}
+systemctl enable "rke2-${INSTALL_RKE2_TYPE}"
 
 if [ "${INSTALL_RKE2_SKIP_START}" = true ]; then
     exit 0
 fi
 
 if [ "${RESTART}" = true ]; then
-    systemctl --no-block restart rke2-${INSTALL_RKE2_TYPE}
+    systemctl --no-block restart "rke2-${INSTALL_RKE2_TYPE}"
 fi
